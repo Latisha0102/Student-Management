f5b0ee22150057c2f4f6de043ef435f76acf0650
Your Name <latishamehra5@gmail.com>
2025-02-17 14:44:14 +0530

    added env

--- db/db.connection.js
+++ db/db.connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+require("dotenv").config()
 const mongoURI = process.env.MONGODB_URI;
 
 const initializeDatabase = async () => {
--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.delete("/students/:id", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
